c49cf90ccd22bd1b880dbcb5e03efb6ded379b53
Ishya Casukhela <casukhelai@gmail.com>
2021-05-25 19:19:47 -0400

    fleshed out javascript plan

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+// Functionality problem
+// NEED: Moment.js library to display the current time and date in the jumbotron
+// GOAL: Time blocks are for standard business and color coded for past,present,future
+// when User clicks into time block, they should be able to type and SAVE the entry
+// then if they refresh the page, entry should remain
+
+// Past: Grey
+// Present: red
+// Future: green
