60f91689e16db45a2443f73f87bd754d50dce8d7
Ishya Casukhela <casukhelai@gmail.com>
2021-05-27 13:43:32 -0400

    updated javascript to add saveBtn feature

--- Develop/script.js
+++ Develop/script.js
@@ -7,3 +7,30 @@
 // Past: Grey
 // Present: red
 // Future: green
+
+// print the today's date in the jumbotron
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+// color code the hours
+
+
+// need an event listener on save button => add function after click
+// .ready is to specify a function to execute when the DOM is loaded
+$(document).ready(function() {
+    // save button saves data even upon refresh
+
+    // .saveBtn eventListener -- $(.saveBtn).on("click",function() 
+    $(".saveBtn").on("click",function() {
+        // get nearby values of the description
+        // get from parent node for the hour div, $(this) returns the current HTML element
+        let hour = $(this).parent().attr("id");
+
+        // get from sibling node from class description (.val gets current value of the first element in set of matched el's)
+        let userInput = $(this).siblings(".description").val(); 
+
+        // localStorage for that data in general
+        localStorage.setItem(hour,userInput);
+    }
+    )}
+)
